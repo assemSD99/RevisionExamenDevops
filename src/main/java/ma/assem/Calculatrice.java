a6ad1dcafd18f24d1676e7b52159fe7ab65696f5
assemSD99 <assem.oujda@gmail.com>
2024-02-02 17:46:12 +0100

    l'ajout du méthode d'addition et soustraction

---
+++ src/main/java/ma/assem/Calculatrice.java
@@ -0,0 +1,12 @@
+package ma.assem;
+
+public class Calculatrice {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int dif(int a,int b){
+        return a-b;
+    }
+    
+}
